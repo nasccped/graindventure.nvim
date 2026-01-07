547e61525f8e574286cd85e2e93041184758de0d
Nasccped <pedro.main.tec@gmail.com>
2026-01-07 18:47:40 -0300

    chore: color palette class + config extra fields

--- lua/graindventure/types.lua
+++ lua/graindventure/types.lua
@@ -1,7 +1,12 @@
 --- @class graindventure.ConfigTable Holds the configuration species.
+--- @field termgui? boolean: If termgui is enabled.
 --- @field bolds? graindventure.ConfigGroups: Which items must be bold.
 --- @field italics? graindventure.ConfigGroups: Which items must be italic.
 --- @field transparents? graindventure.Transparents: Which fields are expected to be transparent.
+--- @field paletteExtend? graindventure.ColorPalette: Color palette to extends into default one.
+--- @field groupsExtend? graindventure.GroupsData: Mapping to extends the default groups.
+
+--- @alias graindventure.GroupsData table<string, string | table>
 
 --- @class graindventure.ConfigGroups Holds all possible groups that a spec can be set.
 --- @field [integer] graindventure.GroupSet: The groups set.
@@ -12,3 +17,19 @@
 --- @field [integer] graindventure.TransparentSet: Fields that are expected to be transparent.
 
 --- @alias graindventure.TransparentSet "bg" | "float" | "terminal"
+
+--- @class graindventure.ColorPalette The color palette class.
+--- @field none? string: "NONE" value.
+--- @field foreground? string: default text foreground color.
+--- @field weakForeground? string: default text foreground color (for weak text).
+--- @field background? string: default background color.
+--- @field strongBackground? string: default background color (for emphasis blocks).
+--- @field black? string: ansi black color.
+--- @field red? string: ansi red color.
+--- @field green? string: ansi green color.
+--- @field yellow? string: ansi yellow color.
+--- @field blue? string: ansi blue color.
+--- @field magenta? string: ansi magenta color.
+--- @field cyan? string: ansi cyan color.
+--- @field white? string: ansi white color.
+--- @field [string]? string: any other color added color.
