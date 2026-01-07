b15901fb0928d7781f9c31407c16affe69be378f
Nasccped <pedro.main.tec@gmail.com>
2026-01-07 18:50:00 -0300

    chore: add color palette candidate

---
+++ lua/graindventure/palette.lua
@@ -0,0 +1,20 @@
+--- @type graindventure.ColorPalette
+local M = {
+  -- defaults
+  none = "NONE",
+  foreground = "#dddede",
+  weakForeground = "##b1b5b5",
+  background = "#040404",
+  strongBackground = "#101010",
+  -- ansis
+  black = "#040404",
+  red = "#d14a33",
+  green = "#5da602",
+  yellow = "#eebb6e",
+  blue = "#417ab3",
+  magenta = "#9c81c7",
+  cyan = "#a9d2e5",
+  white = "#e4e6e6",
+}
+
+return M
